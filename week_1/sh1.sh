71b859b94b66d91af3f5f874c9a59a32b0c5578e
MikkelDilevOlsen1 <s204094@dtu.dk>
2025-02-05 11:50:13 +0100

    new sh

--- week_1/sh1.sh
+++ week_1/sh1.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+### -- job name -- 
 #BSUB -J seleper
+
+### -- que -- 
 #BSUB -q hpc
+
+### -- time -- 
 #BSUB -W 2
+
+### -- memory per core  -- 
 #BSUB -R "rusage[mem=512MB]"
+
+### -- chagne output name -- 
 #BSUB -o sleeper_%J.out
 #BSUB -e sleeper_%J.err
 
--- week_1/sh2.sh
+++ week_1/sh2.sh
@@ -1,11 +1,26 @@
 
 #!/bin/bash
+
+### -- job name -- 
 #BSUB -J seleper
+
+### -- que -- 
 #BSUB -q hpc
+
+### -- time -- 
 #BSUB -W 2
+
+### -- memory per core  -- 
 #BSUB -R "rusage[mem=512MB]"
+
+### -- chagne output name -- 
 #BSUB -o sleeper_%J.out
 #BSUB -e sleeper_%J.err
-#BSUB -notify "start done"
+
+### -- send notification at start -- 
+#BSUB -B 
+
+### -- send notification at completion -- 
+#BSUB -N 
 
 sleep 60
