f6587fbc6db08438549b3c1f4a88fc0fbb9d2306
MikkelDilevOlsen1 <s204094@dtu.dk>
2025-03-12 10:23:22 +0100

    aad sh

---
+++ asigment/sh3_2.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+### -- job name -- 
+#BSUB -J runpython
+
+### -- que -- 
+#BSUB -q hpc
+
+### -- time -- 
+#BSUB -W 8
+
+### -- memory per core  -- 
+#BSUB -R "rusage[mem=1GB]"
+#BSUB -R "span[hosts=1]"
+
+### -- chagne output name -- 
+#BSUB -o opg2_1_%J.out
+#BSUB -e opg2_1_%J.err
+
+#--- Select the model of the CPU  can get list by 'nodestat -F hpc'---
+#BSUB -R "select[model ==  XeonGold6226R]"
+
+### -- number of cores -- 
+#BSUB -n 16
+
+#--- print the hardweare you are using ---
+lscpu
+### -use a specific conda env
+source /dtu/projects/02613_2025/conda/conda_init.sh
+conda activate 02613
+
+python ~/Documents/python-and-hpc/asigment/test.py
+
